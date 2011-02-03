90c12d495667a62c5df99f07c09bc0180212f281
Arash Rouhani <miffoljud@gmail.com>
2011-02-03 10:21:28 +0100

    rensa upp deras skitkod :p

--- Bridge/src/CarWorld.java
+++ Bridge/src/CarWorld.java
@@ -1,3 +1,4 @@
+
 import java.awt.*;
 import java.util.*;
 import javax.swing.*;
@@ -7,16 +8,14 @@ class CarWorld extends JPanel {
     Image bridge;
     Image redCar;
     Image blueCar;
-
     TrafficController controller;
-
     ArrayList<Car> blueCars = new ArrayList<Car>();
     ArrayList<Car> redCars = new ArrayList<Car>();
 
     public CarWorld() {
-	controller = new TrafficController();
+        controller = new TrafficController();
         MediaTracker mt = new MediaTracker(this);
-	Toolkit toolkit = Toolkit.getDefaultToolkit();
+        Toolkit toolkit = Toolkit.getDefaultToolkit();
 
         redCar = toolkit.getImage("image/redcar.gif");
         mt.addImage(redCar, 0);
@@ -33,32 +32,36 @@ class CarWorld extends JPanel {
             System.out.println("Couldn't load one of the images");
         }
 
-	redCars.add(new Car(Car.REDCAR,null,redCar,null));
-	blueCars.add(new Car(Car.BLUECAR,null,blueCar,null));
-        setPreferredSize(new Dimension(bridge.getWidth(null),bridge.getHeight(null)));
+        redCars.add(new Car(Car.REDCAR, null, redCar, null));
+        blueCars.add(new Car(Car.BLUECAR, null, blueCar, null));
+        setPreferredSize(new Dimension(bridge.getWidth(null), bridge.getHeight(null)));
     }
 
-
+    @Override
     public void paintComponent(Graphics g) {
-	g.drawImage(bridge,0,0,this);
-        for (Car c : redCars) c.draw(g);
-        for (Car c : blueCars) c.draw(g);
+        g.drawImage(bridge, 0, 0, this);
+        for (Car c : redCars) {
+            c.draw(g);
+        }
+        for (Car c : blueCars) {
+            c.draw(g);
+        }
     }
 
     public void addCar(final int cartype) {
-	SwingUtilities.invokeLater(new Runnable () {
-		public void run() {
-		    Car c;
-		    if (cartype==Car.REDCAR) {
-			c = new Car(cartype,redCars.get(redCars.size()-1),redCar,controller);
-			redCars.add(c);
-		    } else {
-			c = new Car(cartype,blueCars.get(blueCars.size()-1),blueCar,controller);
-			blueCars.add(c);
-		    }
-		    new Thread(c).start();
-	        }
-	    });
-    }
+        SwingUtilities.invokeLater(new Runnable() {
 
+            public void run() {
+                Car c;
+                if (cartype == Car.REDCAR) {
+                    c = new Car(cartype, redCars.get(redCars.size() - 1), redCar, controller);
+                    redCars.add(c);
+                } else {
+                    c = new Car(cartype, blueCars.get(blueCars.size() - 1), blueCar, controller);
+                    blueCars.add(c);
+                }
+                new Thread(c).start();
+            }
+        });
+    }
 }
--- Bridge/src/Main.java
+++ Bridge/src/Main.java
@@ -1,29 +1,30 @@
+
 public class Main {
 
     private static void nap(int ms) {
-	try {
-	    Thread.sleep(ms);
-	} catch (InterruptedException e) {}
+        try {
+            Thread.sleep(ms);
+        } catch (InterruptedException e) {
+        }
     }
-
 
     public static void main(String[] a) {
-	final CarWindow win = new CarWindow();
+        final CarWindow win = new CarWindow();
 
-	win.pack();
-	win.setVisible(true);
+        win.pack();
+        win.setVisible(true);
 
-	new Thread(new Runnable() {
-		public void run() {
-		    while (true) {
-			nap(25);
-			win.repaint();
-		    }
-		}
-	    }).start();
+        new Thread(new Runnable() {
+
+            public void run() {
+                while (true) {
+                    nap(25);
+                    win.repaint();
+                }
+            }
+        }).start();
 
 
 
     }
-
 }
